3ba6a53887d5cf56c518a5bace9c30e4e8aa6128
Deqing Sun <iamseer@gmail.com>
2016-02-11 10:15:10 -0500

    add code for class 3

---
+++ class03/Arduino_05_595/Arduino_05_595.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(10, LOW);
+  //send data to shift register
+  shiftOut(9, 8, MSBFIRST, 0b11110001); 
+  //create a rising edge to latch data
+  digitalWrite(10, HIGH); 
+  digitalWrite(10, LOW);
+}
