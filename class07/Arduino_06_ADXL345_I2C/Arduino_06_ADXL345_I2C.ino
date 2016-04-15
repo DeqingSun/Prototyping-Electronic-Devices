f77a262f6c86fefabb0b5f50e583dc75521ff19d
Deqing Sun <iamseer@gmail.com>
2016-04-15 00:46:07 -0400

    update tap
    kind of work

--- class07/Arduino_06_ADXL345_I2C/Arduino_06_ADXL345_I2C.ino
+++ class07/Arduino_06_ADXL345_I2C/Arduino_06_ADXL345_I2C.ino
@@ -7,14 +7,21 @@
 
 byte _buff[6];
 
-char POWER_CTL = 0x2D;  //Power Control Register
-char DATA_FORMAT = 0x31;
-char DATAX0 = 0x32; //X-Axis Data 0
-char DATAX1 = 0x33; //X-Axis Data 1
-char DATAY0 = 0x34; //Y-Axis Data 0
-char DATAY1 = 0x35; //Y-Axis Data 1
-char DATAZ0 = 0x36; //Z-Axis Data 0
-char DATAZ1 = 0x37; //Z-Axis Data 1
+#define THRESH_TAP  0x1D  //Tap Threshold
+#define  DURATION  0x21  //Tap Duration
+#define  LATENT    0x22  //Tap latency
+#define WINDOW    0x23  //Tap window
+#define POWER_CTL  0x2D  //Power Control Register
+#define DATA_FORMAT  0x31
+#define DATAX0  0x32 //X-Axis Data 0
+#define DATAX1  0x33 //X-Axis Data 1
+#define DATAY0  0x34 //Y-Axis Data 0
+#define DATAY1  0x35 //Y-Axis Data 1
+#define DATAZ0  0x36 //Z-Axis Data 0
+#define DATAZ1  0x37 //Z-Axis Data 1
+#define  TAP_AXES  0x2A  //Axis control for tap/double tap
+#define  INT_ENABLE  0x2E  //Interrupt Enable Control
+#define  INT_SOURCE  0x30  //Source of interrupts
 
 void setup()
 {
@@ -24,14 +31,31 @@ void setup()
 
   //Put the ADXL345 into +/- 8G range by writing the value 0x01 to the DATA_FORMAT register.
   writeTo(DATA_FORMAT, 0x02);
+
+  //Look for taps on the Z axis only.
+  writeTo(TAP_AXES, 0x01);
+  //Set the Tap Threshold to 1g
+  writeTo(THRESH_TAP, 0x10);
+  //Set the Tap Duration that must be reached 5ms
+  writeTo(DURATION, 0x8);
+
+  //100ms Latency before the second tap can occur.
+  writeTo(LATENT, 0x50);
+  writeTo(WINDOW, 0xFF);
+
+  //Enable the Single and Double Taps.
+  writeTo(INT_ENABLE, 0xE0);
+
   //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
   writeTo(POWER_CTL, 0x08);
+
+
 }
 
 void loop()
 {
   readAccel(); // read the x/y/z tilt
-  delay(500); // only read every 0,5 seconds
+  delay(200); // only read every 0.2 seconds
 }
 
 void readAccel() {
@@ -48,7 +72,13 @@ void readAccel() {
   Serial.print(" y: ");
   Serial.print( y );
   Serial.print(" z: ");
-  Serial.println( z );
+  Serial.print( z );
+
+  readFrom( INT_SOURCE, 1, _buff);
+  Serial.print(" Tap: ");
+  if (_buff[0] & (1 << 6)) Serial.print(" single");
+  if (_buff[0] & (1 << 5)) Serial.print(" double");
+  Serial.print('\n');
 }
 
 void writeTo(byte address, byte val) {
