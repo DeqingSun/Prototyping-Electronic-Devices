a7a5b0c56f41dcf6323bdb0d455d298e76d79394
Deqing Sun <iamseer@gmail.com>
2018-10-28 17:09:05 -0400

    add debug2

--- class08/LedClass/LedClass.ino
+++ class08/LedClass/LedClass.ino
@@ -7,10 +7,14 @@ class BlinkLED {
   public:
     BlinkLED(int _ledPin, int _duration) {
       ledPin = _ledPin;
-      pinMode(ledPin, OUTPUT);
       duration = _duration;
       ledState = LOW;
       previousMillis = 0;
+    }
+
+    void init(){
+      pinMode(ledPin, OUTPUT);
+      digitalWrite(ledPin, ledState);
     }
 
     void Update() {
@@ -31,6 +35,7 @@ class BlinkLED {
 BlinkLED led1(13, 500);
 
 void setup() {
+  led1.init();
 }
 
 void loop() {
