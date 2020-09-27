633a0f8a73b2f4f6ce2275759793ce70176ea434
Deqing Sun <iamseer@gmail.com>
2020-09-27 16:00:54 -0400

    Update Arduino_00_pin11on.ino

--- class04/Arduino_00_pin11on/Arduino_00_pin11on.ino
+++ class04/Arduino_00_pin11on/Arduino_00_pin11on.ino
@@ -1,6 +1,8 @@
 void setup() {
-  pinMode(11,OUTPUT);
-  digitalWrite(11,HIGH);
+  pinMode(11, OUTPUT);
+  digitalWrite(11, HIGH);
+  pinMode(10, OUTPUT);
+  digitalWrite(10, LOW);
 }
 
 void loop() {
