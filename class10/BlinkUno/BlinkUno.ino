996a1b8a91f52c30c1fc234d76e8d4c37a21b341
Deqing Sun <iamseer@gmail.com>
2020-11-06 01:15:51 -0500

    Update BlinkUno.ino

--- class10/BlinkUno/BlinkUno.ino
+++ class10/BlinkUno/BlinkUno.ino
@@ -1,5 +1,17 @@
 #include <Arduino.h>
 
+void __attribute__ ((noinline)) testSimpleAdd(){
+  volatile unsigned char b,c;
+  //unsigned char a,b;
+  volatile unsigned char d;
+
+  asm("nop");
+  b=1;
+  c=2;
+  d=b+c;
+  asm("nop");
+}
+
 volatile unsigned char a;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -7,6 +19,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  testSimpleAdd();
+
   digitalWrite(LED_BUILTIN, HIGH); 
   a=1;
   asm("nop");
